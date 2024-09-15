0c01aed41de6aa7fc08aed48279174fc2a43f579
Susan Li <susansemail195@gmail.com>
2024-09-15 12:11:35 -0700

    Add nodemon custom nx script

--- apps/augury-backend/src/main.ts
+++ apps/augury-backend/src/main.ts
@@ -11,7 +11,7 @@ const app = express();
 app.use('/assets', express.static(path.join(__dirname, 'assets')));
 
 app.get('/api', (req, res) => {
-  res.send({ message: 'Welcome to augury-backend!' });
+  res.send({ message: 'Welcome to augury-backend!!!' });
 });
 
 const port = process.env.PORT || 3333;
