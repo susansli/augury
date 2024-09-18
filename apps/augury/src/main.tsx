a4b0f437ab88331a8c5250223bc6c2d928070a2b
Emma MB <emma@meredithblack.com>
2024-09-18 11:58:37 -0700

    Install and setup Chakra UI package

--- apps/augury/src/main.tsx
+++ apps/augury/src/main.tsx
@@ -1,13 +1,28 @@
 import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
-
+import {
+  ChakraBaseProvider,
+  extendBaseTheme,
+  theme as chakraTheme,
+} from '@chakra-ui/react';
 import App from './app/app';
+
+const { Button } = chakraTheme.components;
+
+// Add components to base theme ONLY as needed
+const theme = extendBaseTheme({
+  components: {
+    Button,
+  },
+});
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <StrictMode>
-    <App />
+    <ChakraBaseProvider theme={theme}>
+      <App />
+    </ChakraBaseProvider>
   </StrictMode>
 );
