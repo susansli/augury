92dc978ddb542dac79b1b25185079f5d0772c8c1
Emma MB <emma@meredithblack.com>
2024-09-20 13:26:25 -0700

    Import theme
    Replaced ChakraBaseProvider with ChakraProvider as it's unclear to me how they interact. May re-implement it later

--- apps/augury/src/main.tsx
+++ apps/augury/src/main.tsx
@@ -1,29 +1,18 @@
 import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
 import {
-  ChakraBaseProvider,
-  extendBaseTheme,
-  theme as chakraTheme,
+  ChakraProvider,
 } from '@chakra-ui/react';
 import App from './app/app';
-
-const { Button } = chakraTheme.components;
-
-// Add components to base theme ONLY as needed
-// This prevents loading initial stylesheets we dont need
-const theme = extendBaseTheme({
-  components: {
-    Button,
-  },
-});
+import theme from './theme';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <StrictMode>
-    <ChakraBaseProvider theme={theme}>
+    <ChakraProvider theme={theme}>
       <App />
-    </ChakraBaseProvider>
+    </ChakraProvider>
   </StrictMode>
 );
