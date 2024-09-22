d5671d9fc6bad6251b66a8d8df1026fd53ddf65d
Dakaro Mueller <LukarioMC@gmail.com>
2024-09-22 16:20:21 -0700

    Added semicolons and function brackets to callbacks

--- apps/augury-backend/src/main.ts
+++ apps/augury-backend/src/main.ts
@@ -33,16 +33,16 @@ app.get('/api', (req, res) => {
 const port = process.env.PORT || 3333;
 const server = app.listen(port, () => {
   mongoose.set('strictQuery', false);
-  mongoose
-    .connect(`${process.env.MONGO_URL}`)
-    .catch(() =>
-      console.error("Please configure your environment's MONGO_URL!")
+  mongoose.connect(`${process.env.MONGO_URL}`).catch(() => {
+    console.error(
+      "Initial Connection Failed - Ensure you've configured your environment's MONGO_URL!"
     );
+  });
 
   const db = mongoose.connection;
-  db.on('error', () =>
-    console.error('Could not connect to Mongo - restart the server.')
-  );
+  db.on('error', () => {
+    console.error('Could not connect to Mongo - restart the server.');
+  });
   db.once('open', () => {
     console.log('Connected to MongoDB');
   });
