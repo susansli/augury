363fa8df6707ce3db4283c2298b1c006c788af5f
Dakaro Mueller <LukarioMC@gmail.com>
2024-09-22 14:59:56 -0700

    Moved mongoose/MongoDB connection logic into app.listen callback

--- apps/augury-backend/src/main.ts
+++ apps/augury-backend/src/main.ts
@@ -8,21 +8,6 @@ import express from 'express';
 import * as path from 'path';
 import mongoose from 'mongoose';
 
-mongoose.set('strictQuery', false);
-mongoose.connect(`${process.env.MONGO_URL}`);
-const db = mongoose.connection;
-
-db.on(
-  'error',
-  console.error.bind(
-    console,
-    'Could not connect to Mongo - restart the server.'
-  )
-);
-db.once('open', () => {
-  console.log('Connected to MongoDB');
-});
-
 const app = express();
 
 app.use('/assets', express.static(path.join(__dirname, 'assets')));
@@ -33,6 +18,21 @@ app.get('/api', (req, res) => {
 
 const port = process.env.PORT || 3333;
 const server = app.listen(port, () => {
+  mongoose.set('strictQuery', false);
+  mongoose
+    .connect(`${process.env.MONGO_URL}`)
+    .catch(() =>
+      console.error("Please configure your environment's MONGO_URL!")
+    );
+
+  const db = mongoose.connection;
+  db.on('error', () =>
+    console.error('Could not connect to Mongo - restart the server.')
+  );
+  db.once('open', () => {
+    console.log('Connected to MongoDB');
+  });
+
   console.log(`Listening at http://localhost:${port}/api`);
 });
 server.on('error', console.error);
