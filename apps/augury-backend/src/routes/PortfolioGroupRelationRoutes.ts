d9d7f8ab342ec18bb152fbec9416377fe24709c6
Dakaro Mueller <LukarioMC@gmail.com>
2024-09-23 15:56:17 -0700

    Remove PortfolioGroupRelation files

--- apps/augury-backend/src/routes/PortfolioGroupRelationRoutes.ts
+++
@@ -1,8 +0,0 @@
-import express, { Router } from 'express';
-export const router: Router = express.Router();
-
-import PortfolioGroupRelationController from '../controllers/auth/PortfolioGroupRelationController';
-
-router
-  .route('/getPortfolioGroupRelation')
-  .get(PortfolioGroupRelationController.getPortfolioGroupRelation);
