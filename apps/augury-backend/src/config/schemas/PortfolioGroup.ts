7a1207e9338a7a3f9e14f455b70eae6f7c24569a
crystal-g77 <129335094+crystal-g77@users.noreply.github.com>
2024-09-22 22:44:18 -0700

    Woops, this references the User not the Portfolio

--- apps/augury-backend/src/config/schemas/PortfolioGroup.ts
+++ apps/augury-backend/src/config/schemas/PortfolioGroup.ts
@@ -16,7 +16,7 @@ const schema = new mongoose.Schema({
   userId: {
     type: mongoose.Schema.Types.ObjectId,
     required: true,
-    ref: 'Portfolio',
+    ref: 'User',
     unique: true,
   },
 });
