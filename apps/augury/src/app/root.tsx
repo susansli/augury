245c123823353d8e30ecb3dacdd0d70e942bb360
Emma MB <emma@meredithblack.com>
2024-09-23 15:38:21 -0700

    Add React-Router-Dom

---
+++ apps/augury/src/app/root.tsx
@@ -0,0 +1,8 @@
+export default function Root(): JSX.Element {
+  return (
+    <>
+      <h1>Hello World!</h1>
+      <div></div>
+    </>
+  );
+}
--- apps/augury/src/main.tsx
+++ apps/augury/src/main.tsx
@@ -1,13 +1,23 @@
 import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-import App from './app/app';
+import ErrorPage from './app/error-page';
+import Root from './app/root';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root />,
+    errorElement: <ErrorPage />,
+  },
+]);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>
 );
