58993b1f2e6ee0d89106dc039f1954d741575b25
Dakaro Mueller <LukarioMC@gmail.com>
2024-09-23 18:43:21 -0700

    Remove session controller & routes

--- apps/augury-backend/src/routes/SessionRoutes.ts
+++
@@ -1,6 +0,0 @@
-import express, { Router } from 'express';
-export const router: Router = express.Router();
-
-import SessionController from '../controllers/auth/SessionController';
-
-router.route('/getSession').get(SessionController.getSession);
