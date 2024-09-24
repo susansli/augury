1ef6433679c6c7d08f7987f8e4fed54bb2785147
crystal-g77 <129335094+crystal-g77@users.noreply.github.com>
2024-09-23 23:09:09 -0700

    getUser method in UserController

--- apps/augury-backend/src/controllers/auth/UserController.ts
+++ apps/augury-backend/src/controllers/auth/UserController.ts
@@ -1,23 +1,27 @@
-import { Request, Response} from "express";
+import { Request, Response } from 'express';
 
-import UserModel from "../../models/auth/UserModel";
+import mongoose from 'mongoose';
+import UserModel from '../../models/auth/UserModel';
 
-const getUser = async (_req: Request, res: Response): Promise<void> => {
+const getUser = async (req: Request, res: Response): Promise<void> => {
+  interface RequestParams {
+    id: string;
+  }
+  const { id }: RequestParams = req.query as unknown as RequestParams;
+  // TODO: Replace once merged in with error handling middleware
+  if (!id) throw new Error('Invalid ID Provided'); // ApiError('Invalid ID Provided');
 
-    // Handle validation of request body  
+  const response = await UserModel.getUser(new mongoose.Types.ObjectId(id));
 
-    const response = UserModel.getUser();
-
-    if (response) {
-        // send the user back after model runs logic
-        res.status(200).send({user: response});
-
-    } else {
-        // we throw an API error since this means something errored out with our server end
-        // throw new ApiError('Unable to retrieve records for this user'); 
-    }
+  if (response) {
+    // send the user back after model runs logic
+    res.status(200).send({ user: response });
+  } else {
+    // we throw an API error since this means something errored out with our server end
+    // throw new ApiError('Unable to retrieve records for this user');
+  }
 };
 
 export default module.exports = {
-    getUser
+  getUser,
 };
