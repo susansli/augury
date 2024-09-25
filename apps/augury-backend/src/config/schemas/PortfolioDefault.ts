139b2c73ee3d7c911d7e4db591a22396f3b6e8d6
Dakaro Mueller <LukarioMC@gmail.com>
2024-09-25 11:52:57 -0700

    Created schema & interface for PortfolioDefaults

---
+++ apps/augury-backend/src/config/schemas/PortfolioDefault.ts
@@ -0,0 +1,27 @@
+import mongoose from 'mongoose';
+import PortfolioDefault from '../interfaces/PortfolioDefault';
+import { PortfolioRisk } from '../enums/PortfolioRisk';
+
+const schema = new mongoose.Schema<PortfolioDefault>({
+  userId: {
+    type: mongoose.Schema.ObjectId,
+    required: true,
+    ref: 'User',
+  },
+
+  name: {
+    type: String,
+    required: true,
+  },
+
+  risk: {
+    type: String,
+    enum: Object.values(PortfolioRisk), // Enum definition
+    default: PortfolioRisk.CONSERVATIVE, // Default value for status
+    required: true,
+  },
+});
+
+const PortfolioDefaultSchema = mongoose.model('Portfolio', schema);
+
+export default PortfolioDefaultSchema;
