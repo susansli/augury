3a6818c0ba12e0c9c4a44265d3fd7396588caedd
crystal-g77 <129335094+crystal-g77@users.noreply.github.com>
2024-09-29 22:14:00 -0700

    Got rid of extra verbs for UserRoute as they are not necessary

--- apps/augury-backend/src/routes/UserRoutes.ts
+++ apps/augury-backend/src/routes/UserRoutes.ts
@@ -3,8 +3,10 @@ export const router: Router = express.Router();
 
 import UserController from '../controllers/auth/UserController';
 
-router.route('/getUser').get(UserController.getUser);
-router.route('/createUser').post(UserController.createUser);
-router.route('/updateUser').put(UserController.updateUser);
-router.route('/updateUserBalance').put(UserController.updateUserBalance);
-router.route('/deleteUser').delete(UserController.deleteUser);
+router
+  .route('/User')
+  .get(UserController.getUser)
+  .post(UserController.createUser)
+  .put(UserController.updateUser)
+  .delete(UserController.deleteUser);
+router.route('/User/Balance').put(UserController.updateUserBalance);
