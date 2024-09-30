6d5b5a7623a9cd81d24a3a8e7e7699b6ee23674a
Susan Li <susansemail195@gmail.com>
2024-09-29 21:32:33 -0700

    Figured out custom icons

--- apps/augury/src/components/home/LoginComponents.tsx
+++ apps/augury/src/components/home/LoginComponents.tsx
@@ -1,14 +1,25 @@
-import { Text } from '@chakra-ui/react';
+import { Button, Flex, Text } from '@chakra-ui/react';
+import GoogleIcon from '../../assets/icons/GoogleIcon';
 
 export default function LoginComponents(): JSX.Element {
   return (
-    <>
-      <Text 
-        fontFamily='monospace'
-        fontSize='3xl'
-      >
+    <Flex flexDir="column" alignItems="center" gap="3">
+      <Text fontFamily="monospace" fontSize="3xl">
         augury.ai
-    </Text>
-    </>
+      </Text>
+      <Text fontFamily="monospace" fontSize="sm">
+        invest like a pro, with AI in tow
+      </Text>
+      <Button 
+      marginTop='5'
+        leftIcon={<GoogleIcon />}
+      >
+        <Flex>
+          <Text>
+            Continue With Google
+          </Text>
+        </Flex>
+      </Button>
+    </Flex>
   );
 }
--- apps/augury/src/pages/Home.tsx
+++ apps/augury/src/pages/Home.tsx
@@ -1,13 +1,10 @@
-import { Box, Center} from '@chakra-ui/react';
+import { Box, Center } from '@chakra-ui/react';
 import LoginComponents from '../components/home/LoginComponents';
 
 export default function Home(): JSX.Element {
   return (
     <Box height="100vh" width="100%">
-      <Center
-        height="100%"
-        width="100%"
-      >
+      <Center height="100%" width="100%">
         <LoginComponents />
       </Center>
     </Box>
