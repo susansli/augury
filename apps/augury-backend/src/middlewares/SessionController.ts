933e02262a9ef5b2c8f3de401543e874aa18d34f
Dakaro Mueller <LukarioMC@gmail.com>
2024-10-06 00:00:27 -0700

    Added GoogleUserResult type

--- apps/augury-backend/src/middlewares/SessionController.ts
+++ apps/augury-backend/src/middlewares/SessionController.ts
@@ -34,6 +34,16 @@ interface GoogleTokensResult {
   id_token: string;
 }
 
+interface GoogleUserResult {
+  id: string;
+  email: string;
+  verified_email: boolean;
+  name: string;
+  given_name: string;
+  family_name: string;
+  picture: string;
+}
+
 export async function getGoogleOAuthTokens(
   code: string
 ): Promise<GoogleTokensResult> {
@@ -68,9 +78,12 @@ export async function getGoogleOAuthTokens(
   }
 }
 
-export async function getGoogleUser(id_token: string, access_token: string) {
+export async function getGoogleUser(
+  id_token: string,
+  access_token: string
+): Promise<GoogleUserResult> {
   try {
-    const res = await axios.get(
+    const res = await axios.get<GoogleUserResult>(
       `https://www.googleapis.com/oauth2/v1/userinfo?alt=json&access_token=${access_token}`,
       {
         headers: {
