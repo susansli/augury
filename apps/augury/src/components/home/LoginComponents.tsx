f27a02f1c93d9536664e1944bf5a852433ae27d1
Susan Li <susansemail195@gmail.com>
2024-10-06 18:18:27 -0700

    Temp link Google Button with navigation

--- apps/augury/src/components/home/LoginComponents.tsx
+++ apps/augury/src/components/home/LoginComponents.tsx
@@ -1,7 +1,9 @@
 import { Button, Flex, Text } from '@chakra-ui/react';
 import GoogleIcon from '../../assets/icons/GoogleIcon';
+import { useNavigate } from 'react-router-dom';
 
 export default function LoginComponents(): JSX.Element {
+  const navigate = useNavigate();
   return (
     <Flex flexDir="column" alignItems="center" gap="3">
       <Text color="text.header" fontFamily="monospace" fontSize="3xl">
@@ -10,7 +12,13 @@ export default function LoginComponents(): JSX.Element {
       <Text color="text.subtitle0" fontFamily="monospace" fontSize="sm">
         invest like a pro, with AI in tow
       </Text>
-      <Button color="text.body" bgColor="background.surface0" marginTop="5" leftIcon={<GoogleIcon />}>
+      <Button
+        color="text.body"
+        bgColor="background.surface0"
+        marginTop="5"
+        leftIcon={<GoogleIcon />}
+        onClick={() => navigate('/onboarding')}
+      >
         <Flex>
           <Text>Continue With Google</Text>
         </Flex>
