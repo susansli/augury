873aaefa4db251a7453f0457ae08de130b1c040a
Dakaro Mueller <LukarioMC@gmail.com>
2024-10-08 10:09:28 -0700

    Refactored Middleware to simply check for session, do not attach `User`. Removed middleware from userRoutes until we decide where to utilize

--- apps/augury-backend/src/middlewares/AttachUserHandler.ts
+++ apps/augury-backend/src/middlewares/AttachUserHandler.ts
@@ -1,25 +1,20 @@
-import { Response, NextFunction } from 'express';
+import { Request, Response, NextFunction } from 'express';
 import { verifyJwt } from '../config/utils/jwt';
 import ApiError from '../errors/ApiError';
+import ClientError from '../errors/ClientError';
 import StatusCode from '../config/enums/StatusCode';
 import Severity from '../config/enums/Severity';
 import Session from '../config/interfaces/Session';
 import SessionModel from '../models/auth/SessionModel';
-import UserModel from '../models/auth/UserModel';
-import RequestWithUser from '../config/types/RequestWithUser';
 
-export async function verifyTokenAndAttachUser(
-  req: RequestWithUser,
+export async function verifyAccessToken(
+  req: Request,
   _res: Response,
   next: NextFunction
 ) {
   const accessToken = req?.cookies?.accessToken;
   if (!accessToken) {
-    throw new ApiError(
-      'No access token found.',
-      StatusCode.UNAUTHORIZED,
-      Severity.LOW
-    );
+    throw new ClientError('No access token found.', StatusCode.UNAUTHORIZED);
   }
 
   try {
@@ -33,12 +28,13 @@ export async function verifyTokenAndAttachUser(
     }
     const token = verificationResult.decoded.session;
     const session: Session = await SessionModel.getSessionByToken(token);
-    // Attach the data and pass control to the next middleware/handler
-    req.user = await UserModel.getUser(session.userId);
+    if (!session) {
+      throw new Error(); // Invalid Session
+    }
     next();
   } catch (error) {
     throw new ApiError(
-      'Invalid token structure.',
+      'Invalid session token or incorrect token structure.',
       StatusCode.FORBIDDEN,
       Severity.MED
     );
--- apps/augury-backend/src/routes/UserRoutes.ts
+++ apps/augury-backend/src/routes/UserRoutes.ts
@@ -1,12 +1,14 @@
 import express, { Router } from 'express';
 import UserController from '../controllers/auth/UserController';
-import { verifyTokenAndAttachUser } from '../middlewares/AttachUserHandler';
+// import { verifyAccessToken } from '../middlewares/AttachUserHandler';
 import asyncErrorHandler from '../middlewares/AsyncErrorHandler';
 
 export const userRouter: Router = express.Router();
 const baseURL = '/user';
 
-userRouter.use(asyncErrorHandler(verifyTokenAndAttachUser));
+// Uncomment if we want to verify the client's auth token for all routes
+// Don't forget to remove redundant calls from routes below as well.
+// userRouter.use(asyncErrorHandler(verifyAccessToken));
 
 userRouter
   .route(baseURL)
