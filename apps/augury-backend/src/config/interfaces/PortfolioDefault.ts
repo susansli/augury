a8588e29bc5ca3d0670d85f76b584ec2cf41d475
Dakaro Mueller <LukarioMC@gmail.com>
2024-10-09 17:26:57 -0700

    Changed types to potentially be strings (they are used for typing requests as well)

--- apps/augury-backend/src/config/interfaces/PortfolioDefault.ts
+++ apps/augury-backend/src/config/interfaces/PortfolioDefault.ts
@@ -2,5 +2,5 @@ import { Types } from 'mongoose';
 import Portfolio from './Portfolio';
 
 export default interface PortfolioDefault extends Portfolio {
-  userId: Types.ObjectId;
+  userId: Types.ObjectId | string;
 }
