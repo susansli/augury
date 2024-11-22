c2a02ad7f176aecd78fe5bceef0ed7bb0d6551dc
Dakaro Mueller <LukarioMC@gmail.com>
2024-11-21 17:43:28 -0800

    Implemented Basic stock model for buyRecord

---
+++ apps/augury-backend/src/models/auth/StockModel.ts
@@ -0,0 +1,60 @@
+import ApiError from '../../errors/ApiError';
+import StatusCode from '../../config/enums/StatusCode';
+import Severity from '../../config/enums/Severity';
+import DocumentId from '../../config/interfaces/DocumentId';
+import SchemaErrorHandler from '../../middlewares/SchemaErrorHandler';
+import StockSchema from '../../config/schemas/StockSchema';
+import Stock from '../../config/interfaces/Stock';
+import BuyRecordSchema from '../../config/schemas/BuyRecordSchema';
+import BuyRecord from '../../config/interfaces/BuyRecord';
+
+/**
+ * Creates a buy record (purchases/sells a stock) for a portfolio
+ * @param portfolioId Mongoose document id of the associated portfolio
+ * @param symbol String ticker symbol of the stock to buy
+ * @param quantity Float amount of shares purchased/sold (Positive or negative)
+ * @param price Float current price of stock. Used for later portfolio evaluation
+ * @returns a `BuyRecord` document
+ * @throws `ApiError` if the stock could not be purchased
+ */
+const createBuyRecord = async (
+  portfolioId: DocumentId,
+  symbol: string,
+  quantity: number,
+  price: number
+) => {
+  // Get stock ID from portfolio ID
+  const stock: Stock = {
+    portfolioId,
+    symbol,
+  };
+  let stockEntry = await SchemaErrorHandler(StockSchema.findOne(stock));
+  if (!stockEntry) {
+    // Entry doesn't exist for this portfolio, create new stock entry
+    stockEntry = await SchemaErrorHandler(StockSchema.create(stock));
+  }
+
+  // Create buy record from stock ID and passed arguments
+  const buyRecord: BuyRecord = {
+    stockId: stockEntry._id,
+    shares: quantity, // Quantity may be +/-
+    boughtAtPrice: price,
+  };
+  const buyRecordEntry = await SchemaErrorHandler(
+    BuyRecordSchema.create(buyRecord)
+  );
+
+  if (!buyRecordEntry) {
+    throw new ApiError(
+      `Unable to ${quantity > 0 ? 'buy' : 'sell'} stock`,
+      StatusCode.INTERNAL_ERROR,
+      Severity.MED
+    );
+  }
+
+  return buyRecordEntry;
+};
+
+export default module.exports = {
+  createBuyRecord,
+};
