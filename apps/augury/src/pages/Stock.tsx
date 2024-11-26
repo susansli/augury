f77c3f39b3e57c287583e62abb9fa758b28e41de
Susan Li <susansemail195@gmail.com>
2024-11-25 17:17:08 -0800

    Define stock card interface and placeholder data

--- apps/augury/src/pages/Stock.tsx
+++ apps/augury/src/pages/Stock.tsx
@@ -2,8 +2,36 @@ import { useEffect, useState } from 'react';
 import Stocks, { StockSymbolInterface } from '../api/stock/Stocks';
 import toast from 'react-hot-toast';
 import AddButton from '../components/generic/AddButton';
-import { Box, Flex, useDisclosure } from '@chakra-ui/react';
+import { Divider, Flex, FormLabel, useDisclosure } from '@chakra-ui/react';
 import BuyStockModal from '../components/stocks/BuyStockModal';
+
+export interface StockCard {
+  symbol: string;
+  currentStockValue: number;
+  totalShares: number;
+  percentageChange: number;
+}
+
+const placeHolderStockData: StockCard[] = [
+  {
+    symbol: 'AA',
+    currentStockValue: 1502.43,
+    totalShares: 100,
+    percentageChange: 5.67,
+  },
+  {
+    symbol: 'AAPL',
+    currentStockValue: 5982.67,
+    totalShares: 99,
+    percentageChange: -0.56,
+  },
+  {
+    symbol: 'TSLA',
+    currentStockValue: 12134.74,
+    totalShares: 502,
+    percentageChange: 20.45,
+  },
+];
 
 export default function Stock(): JSX.Element {
   const [stocks, setStocks] = useState<StockSymbolInterface[]>([]);
@@ -11,7 +39,7 @@ export default function Stock(): JSX.Element {
   const { isOpen, onOpen, onClose } = useDisclosure();
 
   useEffect(() => {
-   void fetchAllStocks();
+    // void fetchAllStocks();
   }, []);
 
   async function fetchAllStocks(): Promise<void> {
@@ -20,7 +48,6 @@ export default function Stock(): JSX.Element {
     if (!response) {
       toast.error('Could not fetch stock symbols, please refresh the page.');
     } else {
-      console.log('response: ', response);
       setStocks(response);
     }
   }
@@ -33,6 +60,10 @@ export default function Stock(): JSX.Element {
         aria-label="Open Stock Modal"
         aira-label={''}
       />
+      <FormLabel color="text.header" fontSize="28" fontWeight="bold">
+        Stocks Held
+      </FormLabel>
+      <Divider />
     </Flex>
   );
 }
